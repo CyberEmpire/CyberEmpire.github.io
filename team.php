d71932d789d9a22a749bb5a1b8fa125e3b0e6577
SevenProvi <74249841+TheCyberEmpire@users.noreply.github.com>
2021-10-25 19:44:37 +0200

    Add files via upload

---
+++ team.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Cyber Empire | Staff</title>
+        <link rel="icon" type="image/x-icon" href="afb/cyberempire.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165370785-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-165370785-1');
+        </script>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="afb/cyberempire.png" style="width: 150px; height: auto;" /></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.html">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-heading text-uppercase">The Staff</div>
+            </div>
+        </header>
+        <!-- Team-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Cyber Empire Staff</h2>
+                    <h3 class="section-subheading text-muted">The Team</h3>
+                </div>
+                <!-- Administrators -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/sevenprovi.png" alt="7Provi" />
+                            <h4>7Provi</h4>
+                            <p style="display: inline;" class="text-muted">Founder</p>
+                            <p class="text-muted">sevenprovi@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/TheCyberEmpire" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCVn_9X30syB8Qbhd6eUXTDA" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="https://discord.gg/wKJaE9C" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/art3mis.png" alt="Art3mis" />
+                            <h4>Art3mis</h4>
+                            <p style="display: inline;" class="text-muted">Administrator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/l8sf.jpg" alt="L8SF" />
+                            <h4>L8SF</h4>
+                            <p style="display: inline;" class="text-muted">Administrator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/futuristic.jpg" alt="Futuristic" />
+                            <h4>Futuristic</h4>
+                            <p style="display: inline;" class="text-muted">Moderator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/TheCyberEmpire" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCVn_9X30syB8Qbhd6eUXTDA" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="https://discord.gg/wKJaE9C" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <br />
+                    <!-- Moderators 1 -->
+                    <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/borat.png" alt="Borat" />
+                            <h4>Borat</h4>
+                            <p style="display: inline;" class="text-muted">Moderator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/chis.webp" alt="Chis" />
+                            <h4>Chis</h4>
+                            <p style="display: inline;" class="text-muted">Moderator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="afb/dreadlord.webp" alt="DR3DL0RD" />
+                            <h4>DR3DL0RD</h4>
+                            <p style="display: inline;" class="text-muted">Moderator</p>
+                            <p class="text-muted">staff@thecyberempire.com</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/TheCyberEmpire" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCVn_9X30syB8Qbhd6eUXTDA" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="https://discord.gg/wKJaE9C" target="_blank"><i class="fab fa-discord"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <div id="success"></div>
+                <a href="index.html"><button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Go Back</button></a>
+            </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 text-lg-left">©2020 thecyberempire.com | All rights reserved </div>
+                    <div class="col-lg-4 my-3 my-lg-0">
+                        <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/TheCyberEmpire" target="_blank"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCVn_9X30syB8Qbhd6eUXTDA" target="_blank"><i class="fab fa-youtube"></i></a><a class="btn btn-dark btn-social mx-2" href="https://discord.gg/wKJaE9C" target="_blank"><i class="fab fa-discord"></i></a>
+                    </div>
+                    <div class="col-lg-4 text-lg-right"><a class="mr-3" href="#!" style="color: #00FF00;">Privacy Policy</a><a href="#!" style="color: #00FF00;">Terms of Use</a></div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
